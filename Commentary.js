f422923599fefe9d6b05ba67d337e78ee7ecbcdc
Luis D. Zamora <zamorald@users.noreply.github.com>
2021-08-15 23:41:33 -0500

    Adopting the W3 Schools look-and-feel
    more info on this theme at: https://www.w3schools.com/w3css/default.asp

---
+++ Commentary.js
@@ -0,0 +1,23 @@
+var elem = document.documentElement;
+
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+/* <button onclick="openFullscreen();">Open Fullscreen</button> */
+
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
+}
+/* <button onclick="closeFullscreen();">Close Fullscreen</button> */
